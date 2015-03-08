6f1791fc0f8490504d05443cb0a992ee96818e8f
ANINDO CHAKRABORTY <anindo78@users.noreply.github.com>
2015-03-08 09:33:39 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,25 @@
+
+unzip("exdata-data-household_power_consumption.zip")
+
+library(dplyr)
+data <- read.table("household_power_consumption.txt", header=T, sep=";", 
+            na.string= "?")
+
+data$datetime <- paste(data$Date, data$Time, sep=" ")
+data$datetime <- strptime(data$datetime, format="%d/%m/%Y %H:%M:%S")
+data$caldate <-  as.Date(data$datetime, format="%d/%m/%Y")
+head(data)
+
+
+
+subset_data <- subset(data[,-1:-2], caldate >= "2007-02-01" & 
+                                    caldate <= "2007-02-02")
+rm(list=c("data"))
+
+png("plot1.png", width = 480, height = 480)
+hist(subset_data$Global_active_power, freq=T, col="red", main="Global Active Power",
+            xlab="Global Active Power (kilowatts)",
+            ylab="Frequency",
+            plot=T)
+         
+dev.off()
